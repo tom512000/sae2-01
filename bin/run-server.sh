998e0346ab56685b434431336faaff85a394a49c
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2023-06-05 16:34:22 +0200

    Ajout de l'auto-chargement

--- bin/run-server.sh
+++ bin/run-server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-APP_DIR="$PWD" php -d display_errors -S localhost:8000 -t public/
+APP_DIR="$PWD" php -d display_errors -d auto_prepend_file="$PWD/vendor/autoload.php" -S localhost:8000 -t public/
