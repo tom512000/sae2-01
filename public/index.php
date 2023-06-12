14100e67e7d31da9a92a8fe8c21edb26bfd98a87
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2023-06-12 11:37:02 +0200

    Modification de index.html et affichage de tous les films

--- public/index.php
+++ public/index.php
@@ -1,3 +1,17 @@
 <?php
 
 declare(strict_types=1);
+
+use Entity\Collection\FilmCollection;
+use Html\AppWebPage;
+
+$appwebpage = new AppWebPage("Films");
+
+$tab = FilmCollection::findAll();
+
+for ($i = 0; $i < count($tab); $i++) {
+    $verif = $appwebpage->escapeString($tab[$i]->getTitle());
+    $appwebpage->appendContent("\t<a href='/artist.php?artistId={$tab[$i]->getId()}'>$verif</a><br>\n");
+}
+
+echo $appwebpage->toHTML();
