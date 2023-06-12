76befded9d46c673049a8cc941b6d3cde9c0f609
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2023-06-12 14:10:45 +0200

    Ajout d'affiche, ancre et image (nf) du style.css

--- public/index.php
+++ public/index.php
@@ -11,7 +11,11 @@ $tab = FilmCollection::findAll();
 
 for ($i = 0; $i < count($tab); $i++) {
     $verif = $appwebpage->escapeString($tab[$i]->getTitle());
-    $appwebpage->appendContent("\t<a href='/film.php?artistId={$tab[$i]->getId()}'>$verif</a><br>\n");
+    $appwebpage->appendContent("\t<a href='/film.php?artistId={$tab[$i]->getId()}'>
+                                            <div class='affiche'>
+                                                   <img src='' alt='Image'/><p>$verif</p>
+                                            </div>
+                                         </a><br>\n");
 }
 
 echo $appwebpage->toHTML();
