066fe7be3df32f0f25b525155debb77f7a43b883
Tom Sikora <tom.sikora@etudiant.univ-reims.fr>
2023-06-14 21:58:00 +0200

    Mise à jour de l'index.html et du style.css

--- public/index.php
+++ public/index.php
@@ -9,27 +9,29 @@ use Html\AppWebPage;
 $appwebpage = new AppWebPage("Films");
 
 $appwebpage->appendContent("<div class='home'>
-                <select name='genre'>");
+                    <select name='genre'>");
 
 $genres = Genre::getGenres();
 foreach ($genres as $genre) {
-    $appwebpage->appendContent("                    <option value='{$genre->getId()}'>{$genre->getName()}</option>");
+    $appwebpage->appendContent("
+                        <option value='{$genre->getId()}'>{$genre->getName()}</option>");
 }
 
 $appwebpage->appendContent("
-                </select>
-                \t<a href='create.php'><img src='img/create.png' alt='Logo create'/></a>
+                    </select>
+                    <a href='create.php'><img src='img/create.png' alt='Logo create'/></a>
                 </div>");
 
 $tab = Film::getFilms();
 
 for ($i = 0; $i < count($tab); $i++) {
     $verif = $appwebpage->escapeString($tab[$i]->getTitle());
-    $appwebpage->appendContent("\t<a class='lien' href='/film.php?filmId={$tab[$i]->getId()}'>
-                                            <div class='affiche'>
-                                                   <img class='menu' src='./image.php?posterId={$tab[$i]->getPosterId()}' alt='Image'/>
-                                                   <p>$verif</p>
-                                            </div>
-                                         </a><br>\n");
+    $appwebpage->appendContent("
+                <a class='lien' href='/film.php?filmId={$tab[$i]->getId()}'>
+                    <div class='affiche'>
+                        <img class='menu' src='./image.php?posterId={$tab[$i]->getPosterId()}' alt='Image'/>
+                        <p>$verif</p>
+                    </div>
+                </a>");
 }
 echo $appwebpage->toHTML();
